19c2c3d8d560eb292a3cfe1c4e6e3b37bfa7cbaf
Musaab-Estaifee-Projects <projects.musaab.estaifee@gmail.com>
2024-08-23 22:13:42 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          iPhone 16 Pro
+          iPhone 15 Pro
         </p>
         <div className="md:w-10/12 w-9/12">
           <video autoPlay muted playsInline={true} key={videoSrc}>
